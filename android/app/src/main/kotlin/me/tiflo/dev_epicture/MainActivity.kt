c8445c329a07b385927c8c175084374ea865a055
Florian Louvet <florian.louvet@epitech.eu>
2019-09-30 15:44:20 +0200

    Initial commit

---
+++ android/app/src/main/kotlin/me/tiflo/dev_epicture/MainActivity.kt
@@ -0,0 +1,13 @@
+package me.tiflo.dev_epicture
+
+import android.os.Bundle
+
+import io.flutter.app.FlutterActivity
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    GeneratedPluginRegistrant.registerWith(this)
+  }
+}
